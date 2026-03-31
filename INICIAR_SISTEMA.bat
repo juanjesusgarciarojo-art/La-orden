6ca5eefad1b816947c1c3def202fa5587b1bcbf6
juanjesusgarciarojo-art <juanjesusgarciarojo@gmail.com>
2026-03-31 20:05:05 +0200

    Delete INICIAR_SISTEMA.bat

--- INICIAR_SISTEMA.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ==========================================
-echo INICIANDO SERVIDOR DE LA ORDEN...
-echo ==========================================
-echo Accede a: http://localhost:3000
-echo ==========================================
-npm run dev
-pause
